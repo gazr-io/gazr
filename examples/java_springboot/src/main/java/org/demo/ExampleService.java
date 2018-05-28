7f1692ce58f0fa8f8d04f99c49b6aff1c9b32083
Michel Daviot <michel@daviot.info>
2018-05-28 12:09:36 +0200

    :sparkles: merge with more relevant spring boot example
    from https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples/spring-boot-sample-data-rest

--- examples/java_springboot/src/main/java/org/demo/ExampleService.java
+++ examples/java_springboot/src/main/java/org/demo/ExampleService.java
@@ -1,8 +1,9 @@
 package org.demo;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
